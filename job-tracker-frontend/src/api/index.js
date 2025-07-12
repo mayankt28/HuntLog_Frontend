2034d9a9e5ed51c78706dbe1ac4af29317eb2c04
mayankt28 <mayankt28@gmail.com>
2025-07-12 01:38:21 -0400

    Minor Updates

--- job-tracker-frontend/src/api/index.js
+++ job-tracker-frontend/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5001/api/v1',
+  baseURL: import.meta.env.VITE_API_URL || 'localhost:3000/api/v1/',
   withCredentials: true,
 });
 
